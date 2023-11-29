3fc7d2bd54f0d3837a612e8c740a95e93a0c3950
LeeCh0129 <cksgh5477@gmail.com>
2023-11-29 18:58:28 +0900

    현재 점수 및 랭킹 기능 추가

--- src/pages/Quiz/QuizScreen.js
+++ src/pages/Quiz/QuizScreen.js
@@ -1,85 +1,133 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-const API_KEY = '737fc88d439055fbc420c49a2612c2dd'; // API 키
+const API_KEY = "737fc88d439055fbc420c49a2612c2dd";
 const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=ko-KR`;
-const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500'; // 이미지 기본 URL
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
+
+const saveScore = (newScore) => {
+  const scores = JSON.parse(localStorage.getItem("scores")) || [];
+  scores.push(newScore);
+  localStorage.setItem("scores", JSON.stringify(scores));
+};
+
+const getScores = () => {
+  return JSON.parse(localStorage.getItem("scores")) || [];
+};
+
+const Ranking = () => {
+  const scores = getScores().sort((a, b) => b - a);
+
+  // 순위 부여
+  const rankedScores = scores.map((score, index) => {
+    const rank = index + 1;
+    return { rank, score };
+  });
+
+  return (
+    <div>
+      <h2>랭킹</h2>
+      <ul>
+        {rankedScores.map((scoreData) => (
+          <li key={scoreData.rank}>
+            {scoreData.rank}위: {scoreData.score} 점
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 const QuizScreen = () => {
-    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-    const [score, setScore] = useState(0);
-    const [questions, setQuestions] = useState([]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [score, setScore] = useState(0);
+  const [questions, setQuestions] = useState([]);
 
-    useEffect(() => {
-        fetchQuestions();
-    }, []);
+  useEffect(() => {
+    fetchQuestions();
+  }, []);
 
-    const fetchQuestions = () => {
-        fetch(API_URL)
-            .then(response => response.json())
-            .then(data => {
-                let movies = data.results;
-                let options = shuffleArray(movies.map(movie => movie.title)).slice(0, 3);
+  const fetchQuestions = () => {
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((data) => {
+        let movies = data.results;
+        let options = shuffleArray(movies.map((movie) => movie.title)).slice(
+          0,
+          3
+        );
 
-                let fetchedQuestions = movies.map((movie, index) => {
-                    return {
-                        id: index,
-                        question: `이 영화의 제목은 무엇일까요?`,
-                        answer: movie.title,
-                        posterPath: movie.poster_path,
-                        options: shuffleArray([movie.title, ...options])
-                    };
-                });
+        let fetchedQuestions = movies.map((movie, index) => {
+          return {
+            id: index,
+            question: `이 영화의 제목은 무엇일까요?`,
+            answer: movie.title,
+            posterPath: movie.poster_path,
+            options: shuffleArray([movie.title, ...options]),
+          };
+        });
 
-                setQuestions(shuffleArray(fetchedQuestions));
-            });
-    };
-    const shuffleArray = (array) => {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]]; // ES6 배열 구조 분해 할당을 사용하여 요소 교환
-        }
-        return array;
-    };
+        setQuestions(shuffleArray(fetchedQuestions));
+      });
+  };
 
-    const handleAnswer = (option) => {
-        if (option === questions[currentQuestionIndex].answer) {
-            setScore(score + 1);
-            if (currentQuestionIndex < questions.length - 1) {
-                setCurrentQuestionIndex(currentQuestionIndex + 1);
-            } else {
-                alert(`퀴즈 완료! 당신의 점수는 ${score + 1}/${questions.length}입니다.`);
-                resetGame();
-            }
-        } else {
-            alert(`틀렸습니다. 당신은 ${score}문제를 맞추셨습니다. 대단하시네요!`);
-            resetGame();
-        }
-    };
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
 
-    const resetGame = () => {
-        setScore(0);
-        setCurrentQuestionIndex(0);
-        fetchQuestions(); // 새로운 퀴즈를 로드합니다.
-    };
+  const handleAnswer = (option) => {
+    if (option === questions[currentQuestionIndex].answer) {
+      setScore(score + 1);
+      if (currentQuestionIndex < questions.length - 1) {
+        setCurrentQuestionIndex(currentQuestionIndex + 1);
+      } else {
+        alert(
+          `퀴즈 완료! 당신의 점수는 ${score + 1}/${questions.length}입니다.`
+        );
+        saveScore(score + 1);
+        resetGame();
+      }
+    } else {
+      alert(
+        `틀렸습니다. 당신은 총${score}문제의 정답을 맞추셨습니다. 대단하시네요!`
+      );
+      saveScore(score);
+      resetGame();
+    }
+  };
 
-    return (
-        <div>
-            <h1>영화 퀴즈</h1>
-            {questions.length > 0 && (
-                <div id={"quizContainer"}>
-                    <img     style={{ height: '300px' }}
-                             src={`${IMAGE_BASE_URL}${questions[currentQuestionIndex].posterPath}`} alt="영화 포스터" />
-                    <p>{questions[currentQuestionIndex].question}</p>
-                    {questions[currentQuestionIndex].options.map((option, index) => (
-                        <button key={index} onClick={() => handleAnswer(option)}>
-                            {option}
-                        </button>
-                    ))}
-                </div>
-            )}
-            <button onClick={resetGame}>다시 게임하기</button>
+  const resetGame = () => {
+    setScore(0);
+    setCurrentQuestionIndex(0);
+    fetchQuestions();
+  };
+
+  return (
+    <div>
+      <h1>영화 퀴즈</h1>
+      <p>현재 점수: {score}</p>
+      {questions.length > 0 && (
+        <div id={"quizContainer"}>
+          <img
+            style={{ height: "300px" }}
+            src={`${IMAGE_BASE_URL}${questions[currentQuestionIndex].posterPath}`}
+            alt="영화 포스터"
+          />
+          <p>{questions[currentQuestionIndex].question}</p>
+          {questions[currentQuestionIndex].options.map((option, index) => (
+            <button key={index} onClick={() => handleAnswer(option)}>
+              {option}
+            </button>
+          ))}
         </div>
-    );
+      )}
+      <button onClick={resetGame}>다시 게임하기</button>
+      <Ranking />
+    </div>
+  );
 };
 
 export default QuizScreen;
